5a31f0f46de1c05571c0ab0b8a56c3f46034fd29
eenspia <write.segun@gmail.com>
2020-02-25 13:55:40 +0100

    initalCommit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage { 
+  expense: number= 0; 
+  expenses: {item:string, cost:number}[]=[];
+  constructor(public alertCtrl:AlertController) {}
 
-  constructor() {}
+  
+  addExpense(){
+    this.alertCtrl.create({
+    header:'Add Expense',
+    inputs:[{name:'addExpense', placeholder:'Enter Item'},
+    {name:'costExpense', placeholder:'Enter Cost'}],
+    buttons:[
+      {text:'Cancel', role:'Cancel'},
+      {text:'update', handler:data=> {
+        if(data.addExpense.length> 0 && data.costExpense.length > 0){
+        this.expenses.push({item:data.addExpense, cost:data.costExpense}) 
+       }
+      }
+    }
+      ]
+
+  }).then(alert => alert.present());
+}
 
 }
+
+
